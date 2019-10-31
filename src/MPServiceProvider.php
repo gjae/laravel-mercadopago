6e0042200032b405a4e42300170e60d8251d4aed
Giovanny J. Avila E <gjavilae@gmail.com>
2019-10-30 23:32:10 -0400

    INITIAL COMMIT

---
+++ src/MPServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Gjae\MercadoPago;
+
+use Illuminate\Support\ServiceProvider;
+class MPServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
